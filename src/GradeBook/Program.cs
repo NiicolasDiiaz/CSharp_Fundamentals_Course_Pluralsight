8eeb44f9ce0468ed96c6cecb10e62e75ac838b96
Nicolas Díaz <nicolasfelipediaz@gmail.com>
2022-03-22 15:35:15 -0500

    Added exceptions

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,12 +1,36 @@
 ﻿using GradeBook;
 
 var book = new Book("Scott's Grade Book");
-book.AddGrade(89.1);
-book.AddGrade(90.5);
-book.AddGrade(77.5);
+
+do
+{
+    Console.WriteLine("Please enter a grade or 'Q' to quit");
+    var input = Console.ReadLine();
+
+    if(input == "Q")
+    {
+        break;
+    }
+
+    try
+    {
+        var grade = Convert.ToDouble(input);
+        book.AddGrade(grade);
+    }
+    catch(ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+} while (true);
 
 var stats = book.GetStatistics();
 
 Console.WriteLine($"The lowest grade is {stats.Low}");
 Console.WriteLine($"The highest grade is {stats.High}");
 Console.WriteLine($"The average grade is {stats.Average:N1}");
+Console.WriteLine($"The letter grade is {stats.Letter}");
